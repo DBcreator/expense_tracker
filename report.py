0e86bbe4e816b410fa00f701817dafda20bf4185
DBcreator <45318525+DBcreator@users.noreply.github.com>
2020-03-15 16:07:51 +0300

    Delete useless comments

--- report.py
+++ report.py
@@ -6,13 +6,8 @@ import numpy as np
 db_connection = db.connect('transaction.db')
 
 df = pd.read_sql_query('SELECT * FROM expenses',db_connection)
-print(df.head())
-#print((df['category'] == 'food').count())
 new_df = df.groupby(['category']).sum()
-#labels = df.groupby(['category'])
 new_df.plot.pie(y = 'amount', figsize=(18.5, 10.5),autopct='%1.1f%%', startangle=90)
-#plt.show()
-#fig.set_size_inches(18.5, 10.5)
 plt.savefig('test_diagram', dpi=200)
 
 
