f56927848524164a599d4ac43f2fa71c7a7fc060
DBcreator <45318525+DBcreator@users.noreply.github.com>
2020-05-16 12:00:56 +0300

    Update spent.py
    Изменил класс Transaction. Теперь в нем инициализируется таблица трат пользователей

--- spent.py
+++ spent.py
@@ -1,5 +1,6 @@
 import _sqlite3 as db
 
+
 class Transaction():
 
     def __init__(self):
@@ -10,7 +11,8 @@ class Transaction():
         self.cursor = self.conn.cursor()
         sql_query = '''
            create table if not exists expenses(
-               id integer primary key autoincrement,
+               transaction_id integer primary key,
+               user_id integer,
                amount number,
                category string,
                date string,
@@ -21,25 +23,7 @@ class Transaction():
         # сохраняем изменения
         self.conn.commit()
 
-    def log(self,amount, category = None, message=None):
-        '''
-        Логируем наши транзакции
-        :param amount: число
-        :param category: категория расходов
-        :param message: (опиционально) описание транзакции где была совершена, на что конкретно были потрачены деньги и т.д
-        :return:
-        '''
-        from datetime import datetime
-        date = str(datetime.now())  # сохраняем сегодняшнюю дату
-        data = (amount, category, date, message)
-        self.conn = db.connect('transaction.db')
-        self.cursor = self.conn.cursor()
-        sql_query = 'INSERT INTO expenses VALUES (?, ?, ?, ?)'
-        self.cursor.execute(sql_query, data)
-        # сохраняем изменения
-        self.conn.commit()
-
-    def delete_all_database(self):
+    def delete_users(self):
         # удаляет всю базу данных
         self.conn = db.connect('transaction.db')
         self.cursor = self.conn.cursor()
@@ -49,19 +33,7 @@ class Transaction():
         self.cursor.execute(sql_query)
         self.conn.commit()
 
-
-    '''
-    Пока не знаю как сделать
-    def delete_from_category(self,category):
-        #удаляет все транзакции из выбранной категории
-        self.conn = db.connect('transaction.db')
-        self.cursor = self.conn.cursor()
-        sql_query = 
-        delete from expenses where category = '{}'
-        self.cursor.execute(sql_query)
-        self.conn.commit()'''
-
-    def delete_transaction(self,id):
+    def delete_transaction(self, id):
         self.conn = db.connect('transaction.db')
         self.cursor = self.conn.cursor()
         sql_query = '''
@@ -69,38 +41,3 @@ class Transaction():
         '''
         self.cursor.execute(sql_query)
         self.conn.commit()
-
-
-    def view(self, category = None):
-        '''
-        Функция которая показывает все траты на выбранную категорию,
-        если категория не выюрана, то показывает все совершенные транзакции
-        :param category:  категория расходов
-        :return:
-        '''
-        self.conn = db.connect('transaction.db')
-        self.cursor = self.conn.cursor()
-        if category: # если задана категория, то показываем общую сумму в категории
-            sql_query1 = '''
-            select * from expenses where category = '{}'
-            '''.format(category)
-            sql_query2 = '''
-            select sum(amount) from expenses where category = '{}'
-            '''.format(category)
-        else: #иначе показываем все транзакции и общую сумму по всем транзакциям
-            sql_query1 = '''
-            select * from expenses  
-            '''.format(category)
-            sql_query2 = '''
-            select sum(amount) from expenses
-            '''.format(category)
-
-        self.cursor.execute(sql_query1)
-        result = self.cursor.fetchall()
-        self.cursor.execute(sql_query2)
-        total_sum = self.cursor.fetchone()[0]
-        return total_sum, result
-
-t1 = Transaction()
-t1.log(2000,'food')
-t1.log(1000,'food')
