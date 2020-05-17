6ecd67b123262f03dd3262e3317c1da51641d430
DBcreator <45318525+DBcreator@users.noreply.github.com>
2020-05-17 21:11:41 +0300

    Update f.py

--- f.py
+++ f.py
@@ -11,8 +11,6 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtCore import QSize, Qt
 
 from PyQt5.QtWidgets import QLabel, QInputDialog, QDialog, QGridLayout, QLineEdit, QPushButton, QMessageBox, QWidget
-
-from pie import pie
 from wallet import Wallet
 
 
@@ -20,36 +18,43 @@ class Ui_MainWindow(object):
 
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(1020, 580)
+        MainWindow.setFixedSize(1020, 580)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.listwidget = QtWidgets.QTableWidget(self.centralwidget)
         self.listwidget.setGeometry(QtCore.QRect(0, 0, 511, 541))
         self.listwidget.setObjectName("listwidget")
-        self.listwidget.setColumnCount(5)
-        self.listwidget.setHorizontalHeaderLabels(["id", "Price", "Category", "Date", "Comments"])
+        self.listwidget.setColumnCount(4)
+        self.listwidget.setHorizontalHeaderLabels(["Price", "Category", "Date", "Comments"])
         self.listwidget.horizontalHeaderItem(0).setTextAlignment(Qt.AlignLeft)
         self.listwidget.horizontalHeaderItem(1).setTextAlignment(Qt.AlignHCenter)
         self.listwidget.horizontalHeaderItem(2).setTextAlignment(Qt.AlignRight)
         self.listwidget.horizontalHeaderItem(3).setTextAlignment(Qt.AlignRight)
-        self.listwidget.horizontalHeaderItem(4).setTextAlignment(Qt.AlignRight)
+
+        self.Tabel_view = QtWidgets.QTableWidget(self.centralwidget)
+        self.Tabel_view.setGeometry(QtCore.QRect(511, 180, 509, 365))
+        self.Tabel_view.setColumnCount(2)
+        self.Tabel_view.setHorizontalHeaderLabels(["Name", "Value"])
+        self.Tabel_view.horizontalHeaderItem(0).setTextAlignment(Qt.AlignHCenter)
+        self.Tabel_view.horizontalHeaderItem(1).setTextAlignment(Qt.AlignHCenter)
+
         self.pushButton = QtWidgets.QPushButton(self.centralwidget)
         self.pushButton.setGeometry(QtCore.QRect(920, 0, 93, 28))
         self.pushButton.setObjectName("pushButton")
-        self.pushButton_1 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_1.setGeometry(QtCore.QRect(820, 50, 93, 28))
-        self.pushButton_1.setObjectName("pushButton_1")
-        self.pushButton_1.setText('Statistics')
         self.label = QtWidgets.QLabel(self.centralwidget)
         self.label.setGeometry(QtCore.QRect(530, 30, 77, 16))
         self.label.setObjectName("label")
         self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
         self.lineEdit.setGeometry(QtCore.QRect(510, 0, 113, 22))
         self.lineEdit.setObjectName("lineEdit")
+
+        self.label_commments = QtWidgets.QLabel(self.centralwidget)
+        self.label_commments.setGeometry(QtCore.QRect(730, 30, 113, 22))
+        self.label_commments.setText('Comments')
         # data
-        self.dateEdit = QtWidgets.QDateEdit(self.centralwidget)
-        self.dateEdit.setGeometry(QtCore.QRect(710, 0, 110, 22))
-        self.dateEdit.setObjectName("dateEdit")
+        self.lineEdit_comments = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit_comments.setGeometry(QtCore.QRect(710, 0, 110, 22))
+        self.lineEdit_comments.setObjectName("lineEdit_comments")
         self.lineEdit_2 = QtWidgets.QLineEdit(self.centralwidget)
         self.lineEdit_2.setGeometry(QtCore.QRect(630, 0, 71, 22))
         self.lineEdit_2.setObjectName("lineEdit_2")
@@ -67,47 +72,71 @@ class Ui_MainWindow(object):
         self.retranslateUi(MainWindow)
 
         # wallet name
-
         self.pushButton_AddWallet = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_AddWallet.setGeometry(QtCore.QRect(720, 90, 71, 22))
+        self.pushButton_AddWallet.setGeometry(QtCore.QRect(700, 60, 71, 22))
         self.pushButton_AddWallet.setObjectName("pushButton_AddWallet")
         self.pushButton_AddWallet.setText('add')
 
         self.lineEdit_Value = QtWidgets.QLineEdit(self.centralwidget)
-        self.lineEdit_Value.setGeometry(QtCore.QRect(550, 90, 71, 22))
+        self.lineEdit_Value.setGeometry(QtCore.QRect(520, 60, 71, 22))
         self.lineEdit_Value.setObjectName("lineEdit_Value")
 
         self.lineEdit_name = QtWidgets.QLineEdit(self.centralwidget)
-        self.lineEdit_name.setGeometry(QtCore.QRect(630, 90, 71, 22))
+        self.lineEdit_name.setGeometry(QtCore.QRect(600, 60, 71, 22))
         self.lineEdit_name.setObjectName("lineEdit_name")
 
         self.label_Value = QtWidgets.QLabel(self.centralwidget)
-        self.label_Value.setGeometry(QtCore.QRect(550, 120, 71, 22))
+        self.label_Value.setGeometry(QtCore.QRect(520, 85, 71, 22))
         self.label_Value.setObjectName("label_Value")
         self.label_Value.setText('Сумма')
 
         self.label_NameWallet = QtWidgets.QLabel(self.centralwidget)
-        self.label_NameWallet.setGeometry(QtCore.QRect(630, 120, 93, 22))
+        self.label_NameWallet.setGeometry(QtCore.QRect(600, 85, 93, 22))
         self.label_NameWallet.setObjectName("label_NameWallet")
         self.label_NameWallet.setText('Имя кошелька')
+
+        # Пополение кошелька
+
+        self.label_Status = QtWidgets.QLabel(self.centralwidget)
+        self.label_Status.setGeometry(QtCore.QRect(520, 150, 90, 22))
+        self.label_Status.setObjectName('label_Status')
+        self.label_Status.setText('сумма')
+
+        self.lineEdit_Sum = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit_Sum.setGeometry(QtCore.QRect(520, 120, 71, 22))
+        self.lineEdit_Sum.setObjectName("lineEdit_Sum")
+
+        self.QComboBox_lst = QtWidgets.QComboBox(self.centralwidget)
+        self.QComboBox_lst.setGeometry(QtCore.QRect(600, 120, 100, 22))
+        self.QComboBox_lst.setObjectName("QComboBox_lst")
+
+        self.pushButton_refill = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_refill.setGeometry(QtCore.QRect(710, 120, 71, 22))
+        self.pushButton_refill.setObjectName("pushButton_refill")
+        self.pushButton_refill.setText('Пополнить')
+
         # combo = QComboBox(self)
         self.Wallet_list = QtWidgets.QComboBox(self.centralwidget)
         self.Wallet_list.setGeometry(QtCore.QRect(820, 0, 100, 22))
         self.Wallet_list.setObjectName("Wallet_list")
 
+        self.pushButton_1 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_1.setGeometry(QtCore.QRect(820, 520, 100, 22))
+        self.pushButton_1.setObjectName("pushButton_1")
+        self.pushButton_1.setText('statistic')
+
         self.login = UI_LoginWindow()
         self.login.show()
 
-        #exit
+        # exit
         self.pushButton_Exit = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_Exit.setGeometry(920,520, 71,22)
+        self.pushButton_Exit.setGeometry(920, 520, 71, 22)
         self.pushButton_Exit.setText('Exit')
 
-        self.stat = Statistics()
+        self.stat = Statistic()
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     singal = QtCore.pyqtSignal(list)
-
 
     def send_signal(self):
         self.signal.emit([self.lineEdit_password.text(), self.lineEdit_login.text()])
@@ -244,7 +273,6 @@ class UI_LoginWindow(QWidget):
         self.registration = Ui_RegWindow()
         self.registration.sign_up.connect(self.Exit)
 
-
     def SignUp(self):
         self.close()
         self.registration.show()
@@ -261,11 +289,11 @@ class UI_LoginWindow(QWidget):
         self.lineEdit_login.clear()
 
 
-class Statistics(QWidget):
+class Statistic(QWidget):
     def __init__(self):
         super().__init__()
 
-    def initUI(self,lst):
+    def initUI(self, lst):
         self.setObjectName("Login")
         self.setFixedSize(700, 700)
         self.photo = QtWidgets.QLabel(self)
@@ -273,7 +301,7 @@ class Statistics(QWidget):
         self.photo.setScaledContents(True)
         self.photo.setObjectName('photo')
         self.user = Wallet()
-        self.user.sign_in(lst[0],lst[1])
+        self.user.sign_in(lst[0], lst[1])
         self.user.pie()
         self.photo.setPixmap(QtGui.QPixmap("diagram.png"))
         self.show()
